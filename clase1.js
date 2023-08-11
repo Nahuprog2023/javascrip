98dafca15b28faab8eabafc8b62c8b6e01a0504b
Nahuprog2023 <137500947+Nahuprog2023@users.noreply.github.com>
2023-08-11 08:35:35 -0300

    Add files via upload

---
+++ clase1.js
@@ -0,0 +1,41 @@
+let suma 3 + 3
+console.log("El resultado de la suma es: " + suma)
+
+let resta = 6 - 3
+console.log("El resultado de la resta es: " - resta)
+ 
+let multiplicacion = 3 * 3
+console.log("El rsultado de la multiplicacion es: " * multiplicacion)
+
+let division = 3 / 3
+console.log("El resultado de la division es: " / division)
+
+alert("El resultado de la suma es " + suma)
+alert("El resultado de la resta es " - resta)
+alert("El resultado de la multiplicacion " * multiplicacion)
+alert("El resultado de la division es " / division)
+
+let edadalumno = 2023 -2008
+alert("Mi edad es" + edadalumno)
+
+let edad
+edad = prompt("Ingresa tu año de nacimiento")
+
+let nombrealumno
+
+nombrealumno = prompt("Ingresa tu nombre")
+
+alert("Bienvenido " + nombrealumno)
+
+let nombrealumno = "Nahuel"
+let apellidoalumno = "Diaz"
+ 
+console.log("Bienvenido " + nombrealumno + apellidoalumno)
+
+let diez = 10
+let cuatro = 4
+let resultadomultiplicacion = diez * cuatro
+
+alert("El resultado de la multiplicacion es: " + resultadomultiplicacion)
+console.log("El resultado de la multiplicacion es: " resultadomultiplicacion)
+
